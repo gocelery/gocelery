73a563a85ee44ac3022c9e6d9afd3acb79e3a583
Damien Peltier <damien@ubble.ai>
2019-01-18 11:15:27 +0100

    Update uuid.go

--- uuid.go
+++ uuid.go
@@ -4,5 +4,5 @@ import uuid "github.com/satori/go.uuid"
 
 // generateUUID generates a v4 uuid and returns it as a string
 func generateUUID() string {
-	return uuid.Must(uuid.NewV4()).String()
+	return uuid.Must(uuid.NewV4(), nil).String()
 }
