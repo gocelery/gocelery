8dc6341334fbed34cb0902b45d8f7cbd84c52b13
Richard Vaughan <richard@purplemonkeys.co.uk>
2019-05-07 10:46:44 +0100

    Fix dependancy issue.

--- uuid.go
+++ uuid.go
@@ -5,7 +5,7 @@ import uuid "github.com/satori/go.uuid"
 // generateUUID generates a v4 uuid and returns it as a string
 func generateUUID() string {
 
-	uuid, _ := uuid.NewV4()
+	uuid := uuid.NewV4()
 
 	return uuid.String()
 }
