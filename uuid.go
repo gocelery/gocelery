a7f91394ae05112451c1d3742a12f96b55ea9b9b
Richard Vaughan <richard@purplemonkeys.co.uk>
2019-05-07 10:33:41 +0100

    Fix error handling build issue.

--- uuid.go
+++ uuid.go
@@ -4,5 +4,8 @@ import uuid "github.com/satori/go.uuid"
 
 // generateUUID generates a v4 uuid and returns it as a string
 func generateUUID() string {
-	return uuid.Must(uuid.NewV4()).String()
+
+	uuid, _ := uuid.NewV4()
+
+	return uuid.String()
 }
